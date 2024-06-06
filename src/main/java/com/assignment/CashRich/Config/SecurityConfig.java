7af5be376dd2b68a4308fedb6e339941ecd5dec5
Abhisek Roy <abhisekroy169@gmail.com>
2024-06-06 10:00:51 +0530

    update api not working fine

--- src/main/java/com/assignment/CashRich/Config/SecurityConfig.java
+++ src/main/java/com/assignment/CashRich/Config/SecurityConfig.java
@@ -1,49 +1,32 @@
-//package com.assignment.CashRich.Config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import com.assignment.CashRich.Util.JwtRequestFilter;
-//import com.assignment.CashRich.models.User;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfiguration {
-//
-//    @Autowired
-//    private JwtRequestFilter jwtRequestFilter;
-//
-//    @Autowired
-//    private UserDetailsService userDetailsService;
-//
-//
-//    @Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//		http
-//			.authorizeHttpRequests((requests) -> requests
-//				.requestMatchers("api/signup").permitAll()
-//				.anyRequest().authenticated()
-//			)
-//			.formLogin((form) -> form
-//				.loginPage("api/login")
-//				.permitAll()
-//			)
-//			.logout((logout) -> logout.permitAll());
-//
-//		return http.build();
-//	}
-//
-//
-//
-//}
+package com.assignment.CashRich.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable().cors().disable().authorizeHttpRequests()
+                .requestMatchers("/api/signup","/api/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer();
+
+        return http.build();
+    
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .build();
+    }
+}
